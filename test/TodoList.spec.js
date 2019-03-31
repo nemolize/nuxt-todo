58162aa2e1c45d7535622060298606ef556bb6be
nemoto <nemoto@enfunctor.com>
2019-03-31 19:19:49 +0900

    test: add empty test of TodoList

--- test/TodoList.spec.js
+++ test/TodoList.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import TodoList from '../components/TodoList';
 
-describe('Logo', () => {
+describe('TodoList', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(TodoList)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
