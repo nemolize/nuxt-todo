f44f1467f97c8bc6779aabb596f385e88ff0777f
nemoto <nemoto@enfunctor.com>
2019-04-01 10:32:57 +0900

    test: comment out empty test case

--- test/TodoList.spec.js
+++ test/TodoList.spec.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import TodoList from '../components/TodoList'
 
 describe('TodoList', () => {
-  test('is a Vue instance', () => {
+  xtest('is a Vue instance', () => {
     const wrapper = mount(TodoList)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
