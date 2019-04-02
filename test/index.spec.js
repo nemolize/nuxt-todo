20e3e6719fa1cb2f6b227ce167beffe7810e5e9c
nemoto <nemoto@enfunctor.com>
2019-04-02 16:43:06 +0900

    test: add test for index component

---
+++ test/index.spec.js
@@ -0,0 +1,15 @@
+import { mount } from '@vue/test-utils'
+import index from '../pages/index'
+
+describe('index', () => {
+  let wrapper
+  beforeEach(() =>
+    (wrapper = mount(index, {
+      stubs: {
+        TodoList: true
+      }
+    })))
+  test('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
