3473c063be6f12fe1b4bcf5827407844b5b7e2f7
nemoto <nemoto@enfunctor.com>
2019-04-02 17:15:29 +0900

    feat: add persisting data in localStorage

--- store/todos.js
+++ store/todos.js
@@ -1,12 +1,19 @@
-const initialList = [
-  { id: 1, done: true, name: 'Buy a milk for my boss' },
-  { id: 2, done: false, name: 'Send a mail to a client' }
-]
-export const state = () => ({
-  list: initialList,
-  counter: initialList.length + 1
-})
+const STORAGE_KEY = 'todos'
+const jsonString = localStorage.getItem(STORAGE_KEY)
+const data = jsonString
+  ? JSON.parse(jsonString)
+  : {
+      list: [
+        { id: 1, done: true, name: 'Buy a milk for my boss' },
+        { id: 2, done: false, name: 'Send a mail to a client' }
+      ],
+      counter: 3
+    }
 
+const persist = state =>
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(state))
+
+export const state = () => data
 export const mutations = {
   add(state, name) {
     state.list.push({
@@ -14,9 +21,11 @@ export const mutations = {
       done: false,
       name
     })
+    persist(state)
   },
   remove(state, todo) {
     state.list = state.list.filter(t => t !== todo)
+    persist(state)
   },
   update(state, todo) {
     state.list
